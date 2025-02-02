f55d7cc75d8a2c4af902257313d6034bb794ac08
BzTheRipper <bayzid.baytur.redwan87@gmail.com>
2025-02-03 02:45:50 +0600

    map added

---
+++ mapComtrol.js
@@ -0,0 +1,10 @@
+// mapComtrol.js
+
+// Initialize the map
+var map = L.map('mapid').setView([23.798788, 90.449459], 16);
+
+// Add OpenStreetMap tile layer
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
