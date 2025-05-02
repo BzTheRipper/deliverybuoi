75d0bb4fcf915b46897e08a0645e14d9b0823f9c
BzTheRipper <bayzid.baytur.redwan87@gmail.com>
2025-05-02 21:53:34 +0600

    just developing the web

--- activateWindowsVenv.ps1
+++ activateWindowsVenv.ps1
@@ -3,3 +3,6 @@ cd windowsVenv\Scripts
 .\Activate.ps1
 # Confirm activation
 Write-Host "The virtual environment has been activated."
+Write-Host "Returning to the deliverybuoi directory............."
+cd ../..
+Write-Host "Returned to the deliverybuoi."
